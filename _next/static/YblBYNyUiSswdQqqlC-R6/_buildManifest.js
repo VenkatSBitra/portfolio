0cfa0bf13e007bf8eee17364b57f3b4994b5c9fb
VenkatSBitra <53979144+VenkatSBitra@users.noreply.github.com>
2021-08-27 12:52:04 +0000

    Deploying to gh-pages from @ VenkatSBitra/portfolio@8c477a749d1c64eb759f97db2a698cdc2edc5b4a 🚀

---
+++ _next/static/YblBYNyUiSswdQqqlC-R6/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/428-2355bf9567844919ad07.js","static/css/62bea295f121a9bfd12e.css","static/chunks/pages/index-6a8f64abfd86c3a03955.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
