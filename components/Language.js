205d51d66e3c2e02a4d525ad5e2f9164efdebf8c
VenkatSBitra <venkat.suprabath@iiitb.ac.in>
2021-08-27 18:31:10 +0530

    Setting up for GH Pages

--- components/Language.js
+++ components/Language.js
@@ -1,11 +1,13 @@
 import { motion } from "framer-motion"
 import styles from "../styles/Language.module.css"
 
+const prefix = '/portfolio'
+
 export const Language = props => {
     return (
         <motion.div variants={props.variants} className={styles.main}>
             <motion.div>
-                <img src={props.src} width="60px" height="60px" />
+                <img src={prefix + props.src} width="60px" height="60px" />
             </motion.div>
             <motion.div>
                 {props.title}
